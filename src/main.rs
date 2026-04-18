456c37770cc7a7952930c776d02649bd31e4b761
Jonas Brandstötter <jonas.brandstoetter@gmx.at>
2026-04-18 23:18:22 +0200

    feat: allow specifying which pager to use

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 #![warn(rust_2018_idioms)]
-use std::{io::Cursor, path::PathBuf, thread};
+use std::{
+    env::VarError,
+    fmt::Display,
+    io::stdout,
+    path::PathBuf,
+    process::{Child, Command, Stdio},
+};
 
-use clap::Parser;
+use anyhow::{Context, anyhow};
+use clap::{Parser, ValueEnum};
 use mr::{Manual, info};
 
 #[derive(Parser)]
 struct Cli {
     /// info file to display
     file: PathBuf,
+    /// what pager to use
+    #[arg(long,default_value_t = Pager::Env)]
+    pager: Pager,
+}
+
+#[derive(Clone, Copy, ValueEnum, PartialEq, Eq, PartialOrd, Ord)]
+enum Pager {
+    /// use the program in the $PAGER environment variable, or `less` if it is unset or empty
+    Env,
+    /// don't use a pager
+    None,
+}
+
+impl Display for Pager {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Pager::Env => write!(f, "env")?,
+            Pager::None => write!(f, "none")?,
+        }
+        Ok(())
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -15,27 +43,36 @@ fn main() -> anyhow::Result<()> {
 
     let manual = info::read_nonsplit_manual(&cli.file)?;
 
-    show_manual(&manual)?;
+    show_manual(&manual, cli.pager)?;
 
     Ok(())
 }
 
-fn show_manual<M: Manual>(man: &M) -> anyhow::Result<()> {
-    let mut buf: Vec<u8> = vec![];
-
-    let mut pager = streampager::Pager::new_using_system_terminal()?;
-    pager.set_scroll_past_eof(false);
-    pager.set_read_ahead_lines(usize::MAX / 2);
-
-    man.render(&mut buf)?;
-    pager.add_stream(Cursor::new(buf), man.title())?;
-
-    let pager_thread = thread::spawn(move || -> anyhow::Result<()> {
-        pager.run()?;
-        Ok(())
-    });
-
-    pager_thread.join().unwrap()?;
+fn show_manual<M: Manual>(man: &M, pager: Pager) -> anyhow::Result<()> {
+    match pager {
+        Pager::Env => {
+            let mut sub = start_env_pager()?;
+            man.render(sub.stdin.as_mut().expect("can write to pager's stin"))?;
+            sub.wait()?;
+        }
+        Pager::None => man.render(&mut stdout())?,
+    };
 
     Ok(())
+}
+
+fn start_env_pager() -> anyhow::Result<Child> {
+    let prog = match std::env::var("PAGER") {
+        Ok(v) if v.is_empty() => Ok("less".to_string()),
+        Err(VarError::NotPresent) => Ok("less".to_string()),
+        Err(VarError::NotUnicode(_)) => Err(anyhow!("$PAGER is not valid unicode")),
+        Ok(v) => Ok(v),
+    }?;
+
+    Ok(Command::new("sh")
+        .arg("-c")
+        .arg(&prog)
+        .stdin(Stdio::piped())
+        .spawn()
+        .context(format!("failed to start pager '{}'", prog))?)
 }
